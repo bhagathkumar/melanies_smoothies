166bca392bff80fa97f80f299acf4bb0f5d8e833
Bhagathkumar Bhagavathi <bhagathkumar@gmail.com>
2025-04-05 10:08:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered'+' '+name_on_order, icon="✅")
 
 
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     
